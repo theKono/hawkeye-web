865c4eda86decf6af4729f4e6a763ea0df3a4a83
Matthew Yang <matthew.yang@thekono.com>
2017-12-06 12:00:15 +0800

    Return user object in login api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   private account: string;
   private validator: string;
   private authenticated: boolean = true;
-  private remember: boolean = true;
+  private remember: boolean = false;
 
   constructor(
     private service: AuthenticationService,
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
     if (loginData) {
       this.account = loginData.account;
       this.validator = loginData.validator;
+      this.remember = loginData.remember;
     }
   }
 
@@ -33,14 +34,19 @@ export class LoginComponent implements OnInit {
     localStorage.removeItem('loginData');
 
     if (this.remember) {
-      let loginData = JSON.stringify({ account: this.account, validator: this.validator });
+      let loginData = JSON.stringify({
+        account: this.account, validator: this.validator, remember: this.remember
+      });
       localStorage.setItem('loginData', loginData);
     }
 
     if (this.account && this.validator) {
       this.service.login(this.account, this.validator).subscribe(res => {
-        if (this.authenticated = res) {
+        if (this.authenticated = !!res) {
+          localStorage.setItem('currentUser', JSON.stringify(res));
           this.router.navigate(['/']);
+        } else {
+          localStorage.removeItem('currentUser');
         }
       });
     }
